4fc340baec4c6c9f377049e5f4b0567ffb09a626
itspancakeman <collinjst.onge@gmail.com>
2024-06-18 18:59:28 -0400

    feat: added solution #10

---
+++ public/exercise-10.js
@@ -0,0 +1,11 @@
+const localLangConfig = 'es';
+
+const LANG = localLangConfig || encodeURI;
+
+console.log('Language setting:', LANG);
+
+const userSavedTheme = 'dark';
+
+const USER_THEME = userSavedTheme || 'light'
+
+console.log('User theme setting:', USER_THEME);
