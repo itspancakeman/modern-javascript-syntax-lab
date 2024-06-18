52cf3e6d4a1852cc0f5b4339a26f4319218994ce
itspancakeman <collinjst.onge@gmail.com>
2024-06-18 17:44:18 -0400

    feat: added solution #9

---
+++ public/exercise-9.js
@@ -0,0 +1,11 @@
+// let pizza = 'tasty';
+
+// if (pizza === 'tasty') {
+//   console.log('yum');
+// } else {
+//   console.log('yuck');
+// }
+
+const pizza = 'tasty' ? 'yum' : 'yuck';
+
+console.log(pizza);
